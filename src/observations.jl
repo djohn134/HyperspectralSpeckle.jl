559d2ccc5430afd97ba47115498c4c4b9a2e1fa7
djohn134 <daj1997@comcast.net>
2025-08-10 19:01:02 -0400

    Changed paths to data files

--- src/observations.jl
+++ src/observations.jl
@@ -1,26 +1,27 @@
 import Interpolations: LinearInterpolation, Line
 
 
+const OPTICS_DIR = "$(@__DIR__)/../data/optics"
 const ELEMENT_FILENAMES = Dict(
     ## Filter
-    "Bessell:U"=>"data/optics/Generic_Bessell.U.dat",
-    "Bessell:B"=>"data/optics/Generic_Bessell.B.dat",
-    "Bessell:V"=>"data/optics/Generic_Bessell.V.dat",
-    "Bessell:R"=>"data/optics/Generic_Bessell.R.dat",
-    "Bessell:I"=>"data/optics/Generic_Bessell.I.dat",
+    "Bessell:U"=>"$(OPTICS_DIR)/Generic_Bessell.U.dat",
+    "Bessell:B"=>"$(OPTICS_DIR)/Generic_Bessell.B.dat",
+    "Bessell:V"=>"$(OPTICS_DIR)/Generic_Bessell.V.dat",
+    "Bessell:R"=>"$(OPTICS_DIR)/Generic_Bessell.R.dat",
+    "Bessell:I"=>"$(OPTICS_DIR)/Generic_Bessell.I.dat",
     ## Lens Coatings
-    "Thorlabs:A"=>"data/optics/thorlabs-A.dat",
-    "Thorlabs:B"=>"data/optics/thorlabs-B.dat",
-    "Thorlabs:AB"=>"data/optics/thorlabs-AB.dat",
-    "Thorlabs:MLA-AR"=>"data/optics/thorlabs-mla-ar.dat",
+    "Thorlabs:A"=>"$(OPTICS_DIR)/thorlabs-A.dat",
+    "Thorlabs:B"=>"$(OPTICS_DIR)/thorlabs-B.dat",
+    "Thorlabs:AB"=>"$(OPTICS_DIR)/thorlabs-AB.dat",
+    "Thorlabs:MLA-AR"=>"$(OPTICS_DIR)/thorlabs-mla-ar.dat",
     ## Mirror Coatings
-    "Thorlabs:OAP-P01"=>"data/optics/thorlabs-OAP-45AOI-P01.dat",
-    "Thorlabs:Plano-P01"=>"data/optics/thorlabs-plano-45AOI-P01.dat",
+    "Thorlabs:OAP-P01"=>"$(OPTICS_DIR)/thorlabs-OAP-45AOI-P01.dat",
+    "Thorlabs:Plano-P01"=>"$(OPTICS_DIR)/thorlabs-plano-45AOI-P01.dat",
     ## Dichroics
-    "Thorlabs:DMLP650P-transmitted"=>"data/optics/DMLP650-transmitted.dat",
-    "Thorlabs:DMLP650P-reflected"=>"data/optics/DMLP650-reflected.dat",
-    "Thorlabs:DMLP805P-transmitted"=>"data/optics/DMLP805-transmitted.dat",
-    "Thorlabs:DMLP805P-reflected"=>"data/optics/DMLP805-reflected.dat",
+    "Thorlabs:DMLP650P-transmitted"=>"$(OPTICS_DIR)/DMLP650-transmitted.dat",
+    "Thorlabs:DMLP650P-reflected"=>"$(OPTICS_DIR)/DMLP650-reflected.dat",
+    "Thorlabs:DMLP805P-transmitted"=>"$(OPTICS_DIR)/DMLP805-transmitted.dat",
+    "Thorlabs:DMLP805P-reflected"=>"$(OPTICS_DIR)/DMLP805-reflected.dat",
 )
 
 struct OpticalElement{T<:AbstractFloat}
@@ -64,6 +65,7 @@ struct OpticalElement{T<:AbstractFloat}
 end
 
 struct Detector{T<:AbstractFloat, S<:Real}
+    label::String
     λ::Vector{T}
     λ_nyquist::T
     qe::Vector{T}
@@ -81,11 +83,12 @@ struct Detector{T<:AbstractFloat, S<:Real}
             gain=1.0,
             saturation=1e99,
             exptime=5e-3,
+            label="",
             verb=true,
             FTYPE=Float64,
             DTYPE=FTYPE
         )
-        detector = new{FTYPE, DTYPE}(λ, λ_nyquist, qe, rn, gain, saturation, pixscale, exptime)
+        detector = new{FTYPE, DTYPE}(label, λ, λ_nyquist, qe, rn, gain, saturation, pixscale, exptime)
         if verb == true
             display(detector)
         end
@@ -125,6 +128,7 @@ struct OpticalSystem{T<:AbstractFloat}
 end
 
 mutable struct Observations{T<:AbstractFloat, S<:Real}
+    label::String
     optics::OpticalSystem{T}
     phase_static::Array{T, 3}
     detector::Detector{T, S}
@@ -132,6 +136,7 @@ mutable struct Observations{T<:AbstractFloat, S<:Real}
     D::T
     aperture_area::T
     times::Vector{T}
+    nsubexp::Int64
     nepochs::Int64
     nsubaps::Int64
     nsubaps_side::Int64
@@ -148,18 +153,20 @@ mutable struct Observations{T<:AbstractFloat, S<:Real}
             D=Inf,
             area=pi*(D/2)^2,
             times=[],
+            nsubexp=1,
             nepochs=0,
             nsubaps=0,
             nsubaps_side=1,
             dim=0,
             ϕ_static=[;;;],
+            label="",
             verb=true,
             FTYPE=Float64
         )
         nepochs = (nepochs==0) ? length(times) : nepochs
         DTYPE = gettypes(detector)[2]
         optics.response .*= detector.qe
-        observations = new{FTYPE, DTYPE}(optics, ϕ_static, detector, ζ, D, area, times, nepochs, nsubaps, nsubaps_side, dim)
+        observations = new{FTYPE, DTYPE}(label, optics, ϕ_static, detector, ζ, D, area, times, nsubexp, nepochs, nsubaps, nsubaps_side, dim)
         if verb == true
             display(observations)
         end
@@ -174,7 +181,9 @@ mutable struct Observations{T<:AbstractFloat, S<:Real}
             D=Inf,
             area=pi*(D/2)^2,
             nsubaps_side=1,
+            nsubexp=1,
             ϕ_static=[;;;],
+            label="",
             verb=true,
             FTYPE=Float64
         )
@@ -182,7 +191,7 @@ mutable struct Observations{T<:AbstractFloat, S<:Real}
         entropy = [calculate_entropy(images[:, :, n, t]) for n=1:nsubaps, t=1:nepochs]
         DTYPE = gettypes(detector)[2]
         optics.response .*= detector.qe
-        observations = new{FTYPE, DTYPE}(optics, ϕ_static, detector, ζ, D, area, times, nepochs, nsubaps, nsubaps_side, dim, images, entropy)
+        observations = new{FTYPE, DTYPE}(label, optics, ϕ_static, detector, ζ, D, area, times, nsubexp, nepochs, nsubaps, nsubaps_side, dim, images, entropy)
         if verb == true
             display(observations)
         end
